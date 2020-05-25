4b98b0f17825a8a0fea1b544c77e033b1a72e821
David Brennan <davidwbrennan@hotmail.com>
2020-05-25 12:01:56 +0100

    Add logger to ListModel.

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -12,8 +13,9 @@ namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
-        private readonly IConfiguration config;
+        private readonly IConfiguration _config;
         private readonly IRestaurantData _restaurantData;
+        private readonly ILogger<ListModel> _logger;
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
@@ -22,15 +24,17 @@ namespace OdeToFood.Pages.Restaurants
         public string SearchTerm { get; set; }
 
         public ListModel(IConfiguration config, 
-                         IRestaurantData restaurantData)
+                         IRestaurantData restaurantData,
+                         ILogger<ListModel> logger)
         {
-            this.config = config;
+            _config = config;
             _restaurantData = restaurantData;
+            _logger = logger;
         }
 
         public void OnGet()
-        {            
-            Message = config["Message"];
+        {
+            _logger.LogError("Executing ListModel");
             Restaurants = _restaurantData.GetRestaurantsByName(SearchTerm);
         }
     }
